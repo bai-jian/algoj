ce41ec908bca26d04fc83dd40f7741ed23b205ce
bai-jian <baijian@baijian.me>
2018-08-13 06:48:13 +0800

    update

--- hihocoder.1434.sorting.photo.files/Main.java
+++ hihocoder.1434.sorting.photo.files/Main.java
@@ -1,16 +1,9 @@
-import java.util.*;
+import java.io.*;
 import java.util.regex.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> filenames = new ArrayList<>();
-
-        Scanner in = new Scanner(System.in);
-        while (in.hasNextLine()) {
-            filenames.add(in.nextLine());
-        }
-
-        filenames.stream().skip(1).sorted((filename1, filename2) -> {
+        new BufferedReader(new InputStreamReader(System.in)).lines().skip(1).sorted((filename1, filename2) -> {
             Pattern pattern = Pattern.compile("(\\D+)(\\d+)");
             Matcher matcher1 = pattern.matcher(filename1);
             matcher1.matches();
@@ -21,11 +14,8 @@ public class Main {
             String stringPart2 = matcher2.group(1);
             if (stringPart1.compareTo(stringPart2) != 0)
                 return stringPart1.compareTo(stringPart2);
-            else {
-                int numberPart1 = Integer.valueOf(matcher1.group(2));
-                int numberPart2 = Integer.valueOf(matcher2.group(2));
-                return numberPart1 - numberPart2;
-            }
+            else
+                return Integer.valueOf(matcher1.group(2)).compareTo(Integer.valueOf(matcher2.group(2)));
         }).forEach(System.out::println);
     }
 }
